5edd0dbb5175d871fd25f9af76ebc8abedb1d2d5
Nícholas Eras <48137930+Nicholamais@users.noreply.github.com>
2021-01-07 18:10:04 -0300

    Update Hangman O Jogo.py

--- Hangman O Jogo.py
+++ Hangman O Jogo.py
@@ -48,7 +48,7 @@ def fechar():
 
 def escrever(arg):
     global j, tentativas
-    tentativas += 1
+    tentativas -= 1
     i = 0
     for x in palavra:
         if arg == x:
@@ -62,21 +62,25 @@ def escrever(arg):
     if not arg in palavra:
         j += 1
         palavrajogador.append(arg)
-        widget.config(text="Letras erradas: " + " ,".join(palavrajogador))
-        widget.place(relx=0.49, rely=0.45)
+        widget.config(text="|  Letras erradas: " + " ,".join(palavrajogador))
+        widget.update()
 
     if d == palavra:
         print("Ganhou")
         aviso.config(text="GANHOU")
         aviso.place(relx=0.2, rely=0.4)
-    elif tentativas == len(palavra):
-        aviso.config(text="PERDEU")
 
+        for x in letras.winfo_children():
+            x.config(state='disabled')
+
+    elif tentativas == 0:
+        aviso.config(text="PERDEU")
         aviso.place(relx=0.2, rely=0.4)
 
-    Tentativas.config(text="Tentativas: " + str(len(palavra) - tentativas))
-    print(d)
-    print(palavra)
+        for x in letras.winfo_children():
+            x.config(state='disabled')
+
+    Tentativas.config(text=f"Tentativas: {tentativas}")
 
 
 janela = Tk()
@@ -84,28 +88,32 @@ janela.title("Hangman")
 janela["bg"] = 'sky blue'
 janela.attributes("-fullscreen", True)
 
-nome = Label(janela, text='Hangman')
-nome.place(relx=0.455, rely=0.05)
+nome = Label(janela, text='Hangman', width = 20, height = 20)
+nome.place(relx=0.5, rely=0.1, anchor='center')
 nome.configure(font="-family {Euphorigenic} -size 27 -weight bold -slant italic", bg='skyblue')
 
 d = ['_ '] * len(''.join(palavra))
 e = d
 j = 0
-tentativas = 0
-widget = Label(janela)
+tentativas = 15
 
-resposta = Label(janela, text=palavra)
-resposta.place(relx=0.495, rely=0.4)
+
+resposta = Label(janela, text='dica: '+a)
+resposta.place(relx=0.5, rely=0.4, anchor='center')
 
 resposta2 = Label(janela, text="".join(d))
-resposta2.place(relx=0.495, rely=0.3)
+resposta2.place(relx=0.5, rely=0.3, anchor='center')
 
 aviso = Label(janela)
 
 dados = LabelFrame(janela)
-Tentativas = Label(dados, text="Tentativas: " + str(len(palavra) - j))
-Tentativas.grid(row=1, column=1)
+
+Tentativas = Label(dados, text=f"Tentativas: {tentativas} ")
+Tentativas.grid(row=1, column=1,padx=1)
 Tentativas.update()
+
+widget = Label(dados, text="|  Letras erradas:")
+widget.grid(row=1,column=2,padx=1)
 
 dados.place(relx=0.35, rely=0.45)
 
@@ -113,25 +121,25 @@ letras = LabelFrame(janela, text='Letras')
 
 for num in range(1, 35):
     if num < 13:
-        x = Button(letras, text=(alfa[num - 1]), padx=10, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
+        x = Button(letras, text=(alfa[num - 1]), padx=10,state=NORMAL, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
             row=1, column=num)
 
         num += 1
     if 12 < num < 25:
         opcao = (
-            Button(letras, text=(alfa[num - 1]), padx=10, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
+            Button(letras, text=(alfa[num - 1]), padx=10,state=NORMAL, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
                 row=2, column=num - 12))
 
         num += 1
     if 24 < num < 27:
         opcao = (
-            Button(letras, text=(alfa[num - 1]), padx=10, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
+            Button(letras, text=(alfa[num - 1]), padx=10, state=NORMAL,command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
                 row=3, column=num - 19))
 
         num += 1
     if 26 < num < 35:
         opcao = (
-            Button(letras, text=(alfa[num - 1]), padx=10, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
+            Button(letras, text=(alfa[num - 1]), padx=10,state=NORMAL, command=lambda letra=alfa[num - 1]: escrever(letra)).grid(
                 row=4, column=num - 24))
 
         num += 1
